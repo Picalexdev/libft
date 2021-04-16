de531e5c9f1172b7cc1c426e7a5f1c83ac0cb483
alexpicosuarez <picalex380@gmail.com>
2021-04-16 17:40:15 +0200

    Buen Commit

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: apico-su <apico-su@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 21:00:56 by apico-su          #+#    #+#             */
-/*   Updated: 2021/04/16 15:52:53 by apico-su         ###   ########.fr       */
+/*   Updated: 2021/04/16 17:38:26 by apico-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	ft_memcmp(const void *sx, const void *sy, size_t n)
 	s1 = (unsigned char *) sx;
 	s2 = (unsigned char *) sy;
 	x = 0;
+	if (!n)
+		return (0);
 	while (s1[x] == s2[x] && x < n - 1)
 		x++;
 	return (s1[x] - s2[x]);
